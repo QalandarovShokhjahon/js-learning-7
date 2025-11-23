e3c2d3161602d27345fd32d1ce17fb966e707562
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-23 14:42:53 +0500

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Funktsiya ma'lum bir vazifani bajarish uchun mo'ljallangan kod blokidir. U faqat chaqirilganda ishlaydi.
+
+// Arrow Function → bu qisqa yozuvga ega zamonaviy funksiya bo‘lib, => (arrow) sintaksisi yordamida yoziladi.
+
